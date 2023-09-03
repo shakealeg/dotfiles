7f17292023844ed937efc68d6efc88d7cc8ffeff
shakealeg <123622445+shakealeg@users.noreply.github.com>
2023-09-03 22:35:35 +1000

    Update install.sh

--- ssh/install.sh
+++ ssh/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+echo "Backing up original sshd_config to /etc/ssh/sshd_config.bak..."
 sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+
+echo "Copying new configuration file to /etc/ssh..."
 sudo cp sshd_config /etc/ssh
