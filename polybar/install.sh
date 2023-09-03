5f1f7744cf8eff3f61c2f0afdb7ef103d9e4382a
shakealeg <123622445+shakealeg@users.noreply.github.com>
2023-09-03 22:36:28 +1000

    Update install.sh

--- polybar/install.sh
+++ polybar/install.sh
@@ -3,11 +3,11 @@
 echo "Attempting to remove any existing configuration..."
 rm -rf ~/.config/polybar
 
-echo "Attempting to make a new configuration directory..."
+echo "Attempting to make a new configuration directory at ~/.config/polybar..."
 mkdir ~/.config/polybar
 
-echo "Copying config to configuration directory..."
+echo "Copying config to ~/.config/polybar..."
 cp config.ini ~/.config/polybar
 
-echo "Copying script to configuration directory..."
+echo "Copying script to ~/.config/polybar..."
 cp system-nvidia-smi.sh ~/.config/polybar
