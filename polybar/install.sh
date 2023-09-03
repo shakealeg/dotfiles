39290d521c11a8ba1221c427916e25ba352676c0
shakealeg <123622445+shakealeg@users.noreply.github.com>
2023-09-03 22:41:30 +1000

    Update install.sh

--- polybar/install.sh
+++ polybar/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "Attempting to remove any existing configuration..."
+echo "Attempting to remove any existing configuration from ~/.config/polybar..."
 rm -rf ~/.config/polybar
 
 echo "Attempting to make a new configuration directory at ~/.config/polybar..."
 mkdir ~/.config/polybar
 
-echo "Copying config to ~/.config/polybar..."
+echo "Attempting to copy new configuration to ~/.config/polybar/config.ini..."
 cp config.ini ~/.config/polybar
 
-echo "Copying script to ~/.config/polybar..."
+echo "Attempting to copy script to ~/.config/polybar/system-nvidia-smi.sh..."
 cp system-nvidia-smi.sh ~/.config/polybar
