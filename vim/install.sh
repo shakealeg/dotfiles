8148745ab54b71df6d2cba9efb70f852983b7d84
shakealeg <123622445+shakealeg@users.noreply.github.com>
2023-09-03 22:41:01 +1000

    Update install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -3,5 +3,5 @@
 echo "Attempting to remove any existing configuration from ~/.vimrc..."
 rm -rf ~/.vimrc
 
-echo "Attempting to copy new configuration to ~/.vimrc"
+echo "Attempting to copy new configuration to ~/.vimrc..."
 cp .vimrc ~/.vimrc
